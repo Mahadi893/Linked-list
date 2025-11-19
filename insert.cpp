eb63a602bf43c643d8e6a52d6dbbe57b4cd86bc4
Mahadi893 <mahadirefat100@gmail.com>
2025-11-18 21:14:26 -0800

    Add files via upload

---
+++ insert.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List {
+    Node* head;
+    Node* tail;
+
+public:
+    List() {
+        head = NULL;
+        tail = NULL;
+    }
+
+    void push_front(int val) {
+        Node* newNode = new Node(val);
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    void push_back(int val) {
+        Node* newNode = new Node(val);
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void printList() {
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main() {
+    List l;
+    l.push_front(10);
+    l.push_front(20);
+    l.push_front(30);
+    l.push_back(5);
+    l.printList();
+
+    return 0;
+}
