2fb67efb73c76425c603839ae608bb6b81168545
Mahadi893 <mahadirefat100@gmail.com>
2025-11-21 07:30:23 -0800

    reverse.cpp

--- reverse.cpp
+++ reverse.cpp
@@ -6,11 +6,12 @@ public:
          ListNode* next = NULL;
          while(curr!=NULL)
          {
-            next = curr -> next;
-            curr -> next = prev;
+            next = curr -> next;  // Preserve the next node address in next
+            curr -> next = prev;  // current node is pointing and connecting with previous node
             prev = curr;
             curr = next;
          }
          return prev;
     }
 };
+
