94af8ff3d1e5acf0712fa28489080eed02b805ad
Mahadi893 <mahadirefat100@gmail.com>
2025-12-02 09:39:35 -0800

    reverse.cpp

--- reverse.cpp
+++ reverse.cpp
@@ -1,17 +1,86 @@
-class Solution {
+#include <bits/stdc++.h>
+using namespace std;
+
+// Node structure
+class ListNode {
 public:
-    ListNode* reverseList(ListNode* head) {
-         ListNode* prev = NULL;
-         ListNode* curr = head;
-         ListNode* next = NULL;
-         while(curr!=NULL)
-         {
-            next = curr -> next;  // Preserve the next node address in next
-            curr -> next = prev;  // current node is pointing and connecting with previous node
-            prev = curr;
-            curr = next;
-         }
-         return prev;
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
     }
 };
 
+// Solution class containing reverse function
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next = NULL;
+
+        while (curr != NULL) {
+            next = curr->next;     // Save next node
+            curr->next = prev;     // Reverse pointer
+            prev = curr;           // Move prev forward
+            curr = next;           // Move curr forward
+        }
+
+        return prev;  // New head
+    }
+};
+
+// Function to insert at end
+ListNode* insertEnd(ListNode* head, int val) {
+    ListNode* newNode = new ListNode(val);
+
+    if (head == NULL) {
+        return newNode;
+    }
+
+    ListNode* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+
+    return head;
+}
+
+// Print the linked list
+void printList(ListNode* head) {
+    ListNode* temp = head;
+    while (temp != NULL) {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    ListNode* head = NULL;
+    Solution obj;
+
+    int n;
+    cout << "Enter number of nodes: ";
+    cin >> n;
+
+    cout << "Enter " << n << " values:\n";
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        head = insertEnd(head, x);
+    }
+
+    cout << "\nOriginal List: ";
+    printList(head);
+
+    head = obj.reverseList(head);
+
+    cout << "Reversed List: ";
+    printList(head);
+
+    return 0;
+}
